463c63438b21ea9fcc1f1d08280fb670b36be3e0
wstgtime <denk174286@outlook.com>
2024-02-28 03:15:12 +0800

    add /api/shorturl endpoint

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl', function(req, res) {
+
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
